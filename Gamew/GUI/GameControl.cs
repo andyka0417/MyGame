e3f1fb3566dd059f1f406bd8dab7ac36ef828e12
Andi <andyka0417@gmail.com>
2018-03-16 15:42:06 +0100

    A játék vezérlőjének hozzáadása

---
+++ Gamew/GUI/GameControl.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace GUI
+{
+    /// <summary>
+    /// A FrameworkElement segítségével tudunk különböző objektumokat, elemeket kirajzoltatni a képernyőre.
+    /// </summary>
+    class GameControl: FrameworkElement
+    {
+    }
+}
